bf4bd073eef25ddce960af728f205c6da193fd92
Aakash Malviya <malviyaaakash4509@gmail.com>
2024-02-25 23:07:58 +0530

    sign up button modify

--- CRED/src/components/Navbar/Navbar.jsx
+++ CRED/src/components/Navbar/Navbar.jsx
@@ -15,9 +15,14 @@ function Navbar() {
                     <li>Help</li>
                </ul>
           </div>
-          <div className='flex items-center gap-2'>
+          <div className='flex items-center gap-4'>
                <button className='text-white text-lg font-bold border sticky py-1 px-4 rounded-md  '>Log</button>
-               <button className='text-white bg-orange-700 hover:bg-orange-800  font-medium rounded-lg  px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none text-sm'>Sign-In</button>
+               <button class="signupBtn">
+                    SIGN UP
+               <span class="arrow">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="rgb(183, 128, 255)"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg>
+               </span>
+               </button>
           </div>
     </nav>
   )
