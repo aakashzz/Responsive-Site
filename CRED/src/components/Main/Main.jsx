82336c48c473b5c0ce5fdd6b5ea0f43cfa93a5ea
Aakash Malviya <malviyaaakash4509@gmail.com>
2024-02-26 08:12:47 +0530

    try some idea

--- CRED/src/components/Main/Main.jsx
+++ CRED/src/components/Main/Main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Logo from "../../../public/images/undraw_mobile_pay_re_sjb8.svg";
+import PaetranPad from "../../../public/images/patternpad.svg"
 import "./Header.css";
-function Header() {
+function Main() {
   return (
     <main>
       <div className="p-10 flex gap-4 ">
@@ -9,11 +10,13 @@ function Header() {
           <div className="p-4">
                <p className="text-7xl font-extrabold h-40 text-center">Rewards for paying credit card bills.</p>
                <p className="text-2xl font-bold text-center pt-14">join 9M+ members who win rewards and cashbacks everyday</p>
-              
           </div>
       </div>
+      {/* <img src={PaetranPad} className="p-2 bg-im text-white "> */}
+          
+      </img>
     </main>
   );
 }
 
-export default Header;
+export default Main;
